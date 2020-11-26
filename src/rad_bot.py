edc7682d24b47f6edd97a06370f40a1e1be815b2
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-26 21:40:12 +1100

    Integrate static_config_parser.py module

--- src/rad_bot.py
+++ src/rad_bot.py
@@ -1,37 +1,32 @@
 import mechanicalsoup
 import json
 import discord
-import configparser
+from static_config_parser import StaticConfigParser
 from random import randrange
 from random import randint
 from random import choice
 
 """Easy to use Discord bot.
+
 A Discord bot that takes ...
 Requires a token input in config.ini.
-  
 """
 
 # globals
 SOUND_LIKE_API_URL = 'https://api.datamuse.com/words?sl='
 RHYME_API_URL = 'https://api.datamuse.com/words?rel_rhy='
 
-
-CONFIG_FILE_PATH = 'config/config.ini'
-
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE_PATH)
+config = StaticConfigParser()
 
 PREFIX = config.get('SERVER', 'Prefix')
-
-browser = mechanicalsoup.Browser()
+BROWSER = mechanicalsoup.Browser()
 
 client = discord.Client()
 
 # functions
 def get_random_word(api_url, plain_text):
     url = api_url + plain_text
-    response = browser.get(url)
+    response = BROWSER.get(url)
     data = json.loads(response.text)
     random_num = randrange(len(data))
     random_word = data[random_num]['word']
@@ -47,28 +42,28 @@ async def on_message(message):
     if message.author == client.user:
         return 
 
-    if message.content.startswith(PREFIX + "version"):
-        await message.channel.send("1.2.1")  
+    if message.content.lower().startswith(PREFIX + "version"):
+        await message.channel.send("1.3.1")  
 
-    if message.content.startswith(PREFIX + "help"):
+    if message.content.lower().startswith(PREFIX + "help"):
         await message.channel.send(" Rad Bot currently supports the following commands:\n" \
-                                    "!version, !word like radovan/rado/rad, !radovan/rado/rad rhyme")
+                                    "!version, !word like radovan/rado/rad, !radovan/rado/rad rhyme, !rad meme")
 
-    if message.content == (PREFIX + "rad meme"):
+    if message.content.lower() == (PREFIX + "rad meme"):
         await message.channel.send(file=discord.File('images/image2.png'))
 
-    if message.content == (PREFIX + "word like radovan"):
+    if message.content.lower() == (PREFIX + "word like radovan"):
         await message.channel.send(get_random_word(SOUND_LIKE_API_URL, 'radovan'))
-    if message.content == (PREFIX + "word like rado"):
+    if message.content.lower() == (PREFIX + "word like rado"):
         await message.channel.send(get_random_word(SOUND_LIKE_API_URL, 'rado'))
-    if message.content == (PREFIX + "word like rad"):
+    if message.content.lower() == (PREFIX + "word like rad"):
 
         await message.channel.send(get_random_word(SOUND_LIKE_API_URL, 'rad'))
-    if message.content == (PREFIX + "radovan rhyme"):
+    if message.content.lower() == (PREFIX + "radovan rhyme"):
         await message.channel.send(get_random_word(RHYME_API_URL, 'radovan'))
-    if message.content == (PREFIX + "rado rhyme"):
+    if message.content.lower() == (PREFIX + "rado rhyme"):
         await message.channel.send(get_random_word(RHYME_API_URL, 'rado'))
-    if message.content == (PREFIX + "rad rhyme"):
+    if message.content.lower() == (PREFIX + "rad rhyme"):
         await message.channel.send(get_random_word(RHYME_API_URL, 'rad'))
     
 def main():
