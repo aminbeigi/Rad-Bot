af2f7398bb4583ef9ae3c5db05eaf5e57ba16336
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-26 19:22:06 +1100

    Add some cool commands

--- src/rad_bot.py
+++ src/rad_bot.py
@@ -1,14 +1,19 @@
-import os
-
+import mechanicalsoup
+import json
 import discord
 import configparser
+from random import randrange
+from random import randint
+from random import choice
 
-"""Easy to use reaction Discord bot.
-A Discord bot that takes user messages as input and reacts with emotes.
+"""Easy to use Discord bot.
+A Discord bot that takes ...
 Requires a token input in config.ini.
   
 """
-CONFIG_FILE_PATH = 'src//config.ini'
+API_URL = 'https://api.datamuse.com/words?sp='
+
+CONFIG_FILE_PATH = 'config/config.ini'
 
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE_PATH)
@@ -24,10 +29,25 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return
+        return 
 
+    if message.content.startswith(PREFIX + "words like radovan"):
+        url = API_URL + 'radovan'
+        browser = mechanicalsoup.Browser()
+        response = browser.get(url)
+        data = json.loads(response.text)
+        print(data)
+        random_num = randrange(len(data))
+        random_word = data[random_num]['word']     
+        await message.channel.send(random_word)
+    
     if message.content.startswith(PREFIX + "help"):
-        print(f"halp") 
+        await message.channel.send("""Currently Rad-Bot supports the following commands: 
+        !version, !words like radovan
+        """)
+
+    if message.content.startswith(PREFIX + "version"):
+        await message.channel.send("1.1.0")        
   
 def main():
     client.run(config.get('SERVER', 'Token'))
